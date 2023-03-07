0ed9551365743a693840c13e27ca8ae9a10214fc
Kazuho Ishida <k10700110@gmail.com>
2023-03-07 09:23:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# REACT VITE SSG TEMPLATE
+
+Pre-render the app into static HTML.
+
+run `yarn generate` and then `dist/static` can be served as a static site.
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import TestImg from '../assets/test.png'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>This is vite react ssg template.</p>
+      <img src={TestImg} alt="default image" />
+    </>
+  )
+}
